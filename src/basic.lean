3f55e66b28bdc8d7e09cdbbafa2b523c4ed2f051
Bassem <25056492+BassemSafieldeen@users.noreply.github.com>
2022-07-01 10:19:08 +0200

    fix def of err_exp

--- src/basic.lean
+++ src/basic.lean
@@ -31,7 +31,7 @@ variables {n : ℕ} {kₙ : ℕ → ι}
 (qₙ : (ℕ → ι) → ℝ) [rnd_var qₙ]
 {q : ι → ℝ} [rnd_var_1 q]
 
-lemma log_iid_rw [iid n qₙ q] :
+lemma log_iid [iid n qₙ q] :
 log(qₙ kₙ) = ∑ i in range n, log(q (kₙ i)) := sorry
 
 /-- relative entropy -/
@@ -48,18 +48,17 @@ lemma discrimination_additive_of_iid [iid n qₙ₁ q₁] [iid n qₙ₂ q₂] :
 discrimination qₙ₁ qₙ₂ = n * discrimination_1 q₁ q₂ :=
 begin
   -- calc discrimination q₁ₙ q₂ₙ = ∑ kₙ, (qₙ₁ kₙ) * (log(qₙ₁ kₙ) - log(qₙ₂ kₙ)) : by rw discrimination
-  --                        ... = ∑ kₙ, (∏ i in range n, (q₁ (kₙ n))) * ∑ n, (log(q₁ (kₙ n)) - log(q₂ (kₙ n))) : by rw log_iid_rw
+  --                        ... = ∑ kₙ, (∏ i in range n, (q₁ (kₙ n))) * ∑ i in range n, (log(q₁ (kₙ n)) - log(q₂ (kₙ n))) : by rw log_iid_rw
   --                        ... = n * discrimination_1 q₁ q₂ : by sorry,
   sorry,
 end
 
 /-- How close to q₂ can we get if we stay ε-close to q₁? -/
 def err_exp (qₙ₁ qₙ₂ : (ℕ → ι) → ℝ) [rnd_var qₙ₁] [rnd_var qₙ₂] (ε : ℝ) :=
-Inf { b : ℝ | ∀ qₙ, b = discrimination qₙ qₙ₂ ∧ discrimination qₙ₁ qₙ ≤ ε }
+Inf { b : ℝ | ∃ qₙ, discrimination qₙ₁ qₙ ≤ ε ∧ discrimination qₙ qₙ₂ = b }
 
-/-- How close to q₂ can we get if we stay ε-close to q₁? -/
 def err_exp_1 (q₁ q₂ : ι → ℝ) (ε : ℝ) :=
-Inf { b : ℝ | ∀ q, b = discrimination_1 q q₂ ∧ discrimination_1 q₁ q ≤ ε }
+Inf { b : ℝ | ∃ q, discrimination_1 q₁ q ≤ ε ∧ discrimination_1 q q₂ = b }
 
 /-- can get δ-close to q₂ when ε-close to q₁ -/
 def achieves_err_exp (qₙ qₙ₁ qₙ₂ : (ℕ → ι) → ℝ) [rnd_var qₙ] [rnd_var qₙ₁] [rnd_var qₙ₂] (ε δ : ℝ) :=
