48efcfbc86e2f3f1af8b662cbfc6a71bf787c548
Bassem <25056492+BassemSafieldeen@users.noreply.github.com>
2022-07-01 10:19:36 +0200

    update notation

--- src/lower_bound_lemmas.lean
+++ src/lower_bound_lemmas.lean
@@ -8,7 +8,7 @@ noncomputable theory
 
 variables {ι : Type} [fintype ι] [decidable_eq ι] [fintype (ℕ → ι)]
 (qₙ qₙ₁ qₙ₂ : (ℕ → ι) → ℝ) [rnd_var qₙ₁] [rnd_var qₙ₂] [rnd_var qₙ]
-(r T : ℝ) [r > 0] [T = (err_exp qₙ₁ qₙ₂ r) - r] -- consider this partition
+(r Tₙ : ℝ) [r > 0] [Tₙ = (err_exp qₙ₁ qₙ₂ r) - r] -- consider this partition
 
 variables (δ ε : ℝ) [δ > 0] [ε > 0]
 [achieves_err_exp qₙ qₙ₁ qₙ₂ r δ]
@@ -20,7 +20,7 @@ variables (σₙ₁ σₙ₂ : ℝ)
 variables (q₁ q₂ : ι → ℝ) [rnd_var_1 q₁] [rnd_var_1 q₂]
 
 /-- Corollary 2 in Blahut1974 -/
-theorem prob_of_α_error_ge {n : ℕ} [iid n qₙ₁ q₁] [iid n qₙ₂ q₂] : 
+theorem prob_of_α_error_ge_of_iid {n : ℕ} [iid n qₙ₁ q₁] [iid n qₙ₂ q₂] : 
 ∀ ε > 0, ∀ γ > 0,
-β qₙ₁ qₙ₂ T ≤ γ * exp(-n*(r + ε))
-→ α qₙ₁ qₙ₂ T ≥ exp(-n*(err_exp_1 q₁ q₂ r/n + ε)) * (1 - (σₙ₁ + σₙ₂)/(n*ε^2) - γ) := sorry
+βₙ qₙ₁ qₙ₂ Tₙ ≤ γ * exp(-n*(r + ε))
+→ αₙ qₙ₁ qₙ₂ Tₙ ≥ exp(-n*(err_exp_1 q₁ q₂ r/n + ε)) * (1 - (σₙ₁ + σₙ₂)/(n*ε^2) - γ) := sorry
--- src/upper_bound_lemmas.lean
+++ src/upper_bound_lemmas.lean
@@ -10,21 +10,22 @@ variables {ι : Type} [fintype ι]
 [fintype (ℕ → ι)] [decidable_eq ι]
 (q₁ q₂ : ι → ℝ) [rnd_var_1 q₁] [rnd_var_1 q₂]
 (qₙ₁ qₙ₂ : (ℕ → ι) → ℝ) [rnd_var qₙ₁] [rnd_var qₙ₂]
-(r T : ℝ) [r > 0] [T = (err_exp qₙ₁ qₙ₁ r) - r] -- consider this partition
+(r T Tₙ : ℝ) [r > 0]
+[T = (err_exp_1 q₁ q₁ r) - r] [Tₙ = (err_exp qₙ₁ qₙ₁ r) - r] -- consider this partition
 
 /-- Thm. 9 in Blahut1974: larger r → smaller β -/
-theorem prob_of_β_error_le : β_1 q₁ q₂ T ≤ exp(-r) := sorry
+theorem prob_of_β_error_le : β q₁ q₂ T ≤ exp(-r) := sorry
 
 /-- Thm. 9 in Blahut1974: r-close to q₁ → farther from q₂ → smaller error α -/
-theorem prob_of_α_error_le : α_1 q₁ q₂ T ≤ exp(-err_exp_1 q₁ q₂ r) := sorry
+theorem prob_of_α_error_le : α q₁ q₂ T ≤ exp(-err_exp_1 q₁ q₂ r) := sorry
 
 variables (n : ℕ) [iid n qₙ₁ q₁] [iid n qₙ₂ q₂]
 
 /-- Corollary 1 in Blahut1974 -/
 theorem prob_of_α_error_le_of_iid :
-α qₙ₁ qₙ₂ T ≤ exp(-n * err_exp_1 q₁ q₂ r/n) := sorry
+αₙ qₙ₁ qₙ₂ Tₙ ≤ exp(-n * err_exp_1 q₁ q₂ r/n) := sorry
 -- by simp only [prob_of_α_error_le, err_exp_of_iid]
 
 /-- Corollary 1 in Blahut1974 -/
 theorem prob_of_β_error_le_of_iid :
-β qₙ₁ qₙ₂ T ≤ exp(-n*r) := sorry
+βₙ qₙ₁ qₙ₂ Tₙ ≤ exp(-n*r) := sorry
